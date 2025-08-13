a6ff7a7ede63c2e9ec0f3373e2f5bb9e3936e1b6
SIE <smartemmanuel1234@gmail.com>
2025-08-13 04:44:07 +0100

    Initial commit

---
+++ 01_Data_Collection/multiply.py
@@ -0,0 +1,7 @@
+def funct_multiply():
+    num1 = int(input("enter first number: "))
+    num2 = int(input("enter second number: "))
+    result = num1 * num2
+    print(f"{num1} multiplied by {num2} is {result}")
+
+funct_multiply()
